92d8ade9c1d654c12759c6836e0f4d9b19b72cb4
joaovsz <joaovitor1713coin@gmail.com>
2026-09-20 17:48:46 -0300

    feat(eureka): configurar eureka-server para service discovery na porta 8761

---
+++ eureka-server/src/test/java/br/edu/infnet/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
